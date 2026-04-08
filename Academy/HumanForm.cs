0a1417690b0134f9f29879cc946958eac21af063
Дарья Серова <dashaflay2000@gmail.com>
2026-04-08 19:37:43 +0300

    Student model in progress

--- Academy/HumanForm.cs
+++ Academy/HumanForm.cs
@@ -12,13 +12,16 @@ using System.Configuration;
 
 namespace Academy
 {
-	public abstract partial class HumanForm : Form
+	public partial class HumanForm : Form
 	{
-		public HumanForm()
+		protected HumanForm()
 		{
 			InitializeComponent();
 		}
 
-		protected abstract void buttonOK_Click(object sender, EventArgs e);
+		protected virtual void buttonOK_Click(object sender, EventArgs e)
+		{
+
+		}
 	}
 }
